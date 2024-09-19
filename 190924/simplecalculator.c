b3be94c0e4b9a078b81dac5ff750f5ef5d401dac
OMKAR PRASAD BARAI <omkarprasadbarai7597@gmail.com>
2024-09-19 12:00:00 +0530

    add simple calculator

---
+++ 190924/simplecalculator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    char operator;
+    double num1, num2, result;
+
+    printf("Enter operator (+, -, *, /): ");
+    scanf("%c", &operator);
+    printf("Enter two operands: ");
+    scanf("%lf %lf", &num1, &num2);
+
+    switch (operator) {
+        case '+': result = num1 + num2; break;
+        case '-': result = num1 - num2; break;
+        case '*': result = num1 * num2; break;
+        case '/': result = num1 / num2; break;
+        default: printf("Invalid operator!\n"); return 1;
+    }
+
+    printf("Result: %.2lf\n", result);
+    return 0;
+}
