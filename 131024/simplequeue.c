fb98b52ac186fbc98022538abef33f75157c5107
OMKAR PRASAD BARAI <omkarprasadbarai7597@gmail.com>
2024-10-13 12:00:00 +0530

    adding simple queue

---
+++ 131024/simplequeue.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#define SIZE 5
+
+int queue[SIZE], front = -1, rear = -1;
+
+void enqueue(int value) {
+    if (rear == SIZE - 1)
+        printf("Queue is full.\n");
+    else {
+        if (front == -1)
+            front = 0;
+        queue[++rear] = value;
+        printf("Inserted %d\n", value);
+    }
+}
+
+void dequeue() {
+    if (front == -1 || front > rear)
+        printf("Queue is empty.\n");
+    else
+        printf("Deleted %d\n", queue[front++]);
+}
+
+void display() {
+    if (front == -1 || front > rear)
+        printf("Queue is empty.\n");
+    else {
+        printf("Queue elements are: ");
+        for (int i = front; i <= rear; i++)
+            printf("%d ", queue[i]);
+        printf("\n");
+    }
+}
+
+int main() {
+    enqueue(10);
+    enqueue(20);
+    dequeue();
+    display();
+    return 0;
+}
