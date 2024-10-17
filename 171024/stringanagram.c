73f3f1d9d846307a61454e62a48aaa55d73ee3de
OMKAR PRASAD BARAI <omkarprasadbarai7597@gmail.com>
2024-10-17 12:00:00 +0530

    adding string an agram

---
+++ 171024/stringanagram.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+void sortString(char str[]) {
+    int len = strlen(str);
+    for (int i = 0; i < len - 1; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (str[i] > str[j]) {
+                char temp = str[i];
+                str[i] = str[j];
+                str[j] = temp;
+            }
+        }
+    }
+}
+
+int areAnagrams(char str1[], char str2[]) {
+    if (strlen(str1) != strlen(str2)) return 0;
+    sortString(str1);
+    sortString(str2);
+    return strcmp(str1, str2) == 0;
+}
+
+int main() {
+    char str1[100], str2[100];
+    printf("Enter the first string: ");
+    scanf("%s", str1);
+    printf("Enter the second string: ");
+    scanf("%s", str2);
+
+    if (areAnagrams(str1, str2)) {
+        printf("The strings are anagrams.\n");
+    } else {
+        printf("The strings are not anagrams.\n");
+    }
+
+    return 0;
+}
