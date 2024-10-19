a4d62f775eaca17cf4a8558720d9e8ae11adec5d
OMKAR PRASAD BARAI <omkarprasadbarai7597@gmail.com>
2024-10-19 12:00:00 +0530

    adding uniue number in array

---
+++ 191024/uniquenumber.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter the elements:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Unique elements: ");
+    for (int i = 0; i < n; i++) {
+        int isUnique = 1;
+        for (int j = 0; j < n; j++) {
+            if (i != j && arr[i] == arr[j]) {
+                isUnique = 0;
+                break;
+            }
+        }
+        if (isUnique) {
+            printf("%d ", arr[i]);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
