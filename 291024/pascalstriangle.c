b6d2cfd6171ca8bf70ad5fb6e68bbcee47d962ed
OMKAR PRASAD BARAI <omkarprasadbarai7597@gmail.com>
2024-10-29 12:00:00 +0530

    adding make pascale triangle

---
+++ 291024/pascalstriangle.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int rows;
+    printf("Enter the number of rows for Pascal's triangle: ");
+    scanf("%d", &rows);
+
+    for (int i = 0; i < rows; i++) {
+        int val = 1;
+        for (int j = 0; j <= i; j++) {
+            printf("%d ", val);
+            val = val * (i - j) / (j + 1);
+        }
+        printf("\n");
+    }
+    return 0;
+}
